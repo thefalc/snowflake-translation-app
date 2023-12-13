ac685ba0d7d610751d94693153dadc72096dedad
Sean Falconer <67345830+thefalc@users.noreply.github.com>
2023-12-12 17:04:24 -0800

    Fixed comment formatting

--- src/udf.py
+++ src/udf.py
@@ -9,14 +9,14 @@ import pandas as pd
 
 def init_app(session: Session, config) -> str:
   """
-   Initializes function API endpoints with access to the secret and API integration.
+    Initializes function API endpoints with access to the secret and API integration.
 
-   Args:
-       session (Session): An active session object for authentication and communication.
-       config (Any): The configuration settings for the connector.
+    Args:
+      session (Session): An active session object for authentication and communication.
+      config (Any): The configuration settings for the connector.
 
-   Returns:
-       str: A status message indicating the result of the provisioning process.
+    Returns:
+      str: A status message indicating the result of the provisioning process.
    """
   secret_name = config['secret_name']
   external_access_integration_name = config['external_access_integration_name']
